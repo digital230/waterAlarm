8354781111d976b3a121978472f7e497ac2c341f
digital230 <digit230@gmail.com>
2019-02-10 18:17:30 +0500

    initial

---
+++ src/home/Home.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import styles from './style';
+
+class Home extends Component {
+  constructor(props){
+    super(props);
+  }
+
+
+  render() {
+
+
+    return (
+      <View style={styles.root}>
+
+      </View>
+    )
+  }
+}
+
+export default Home;
---
+++ src/home/style.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  root: {
+    borderWidth: 1,
+    borderColor: 'black',
+    height: 250,
+    width: 250,
+    borderRadius: 150,
+  }
+
+});
