a0670058ac88c2b42b301da1966a4e93dbe006e1
digital230 <digit230@gmail.com>
2019-02-10 20:51:03 +0500

    water wave1

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,20 +1,46 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableHighlight} from 'react-native';
+import Wave from 'react-native-waveview';
 import styles from './style';
 
 class Home extends Component {
   constructor(props){
     super(props);
+    this.state = {}
+    this.interval = null;
+    this.inc = 0;
+  }
+
+  componentDidMount() {
+    this.interval =  setInterval(() => {
+      this.inc = this.inc+10;
+      if(this.inc < 200) {
+        this._waveRect && this._waveRect.setWaterHeight(this.inc);
+      } else {
+        clearInterval(this.interval);
+      }
+    },500)
   }
 
 
   render() {
 
-
     return (
-      <View style={styles.root}>
-
+    <View style={styles.root}>
+      <View style={styles.waveroot}>
+        <Wave
+          ref={ref=>this._waveRect = ref}
+          style={styles.wave}
+          H={1}
+          waveParams={[
+              {A: 10, T: 180, fill: '#62c2ff'},
+              {A: 15, T: 140, fill: '#0087dc'},
+              {A: 20, T: 100, fill: '#1aa7ff'},
+          ]}
+          animated={true}
+        />
       </View>
+    </View>
     )
   }
 }
--- src/home/style.js
+++ src/home/style.js
@@ -7,6 +7,25 @@ export default StyleSheet.create({
     height: 250,
     width: 250,
     borderRadius: 150,
-  }
+  },
+  waveroot: {
+        flex: 1,
+        marginVertical: 10,
+        marginHorizontal: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    wave: {
+        width: 250,
+        aspectRatio: 1,
+        overflow: 'hidden',
+        borderRadius: 150,
+    },
+    waveBall: {
+        width: 100,
+        aspectRatio: 1,
+        borderRadius: 50,
+        overflow: 'hidden',
+    }
 
 });
